06267ea9efb67f6206d852f55197a63d0ae50fa5
MicheleDeF <defalco_michele@libero.it>
2022-09-24 14:00:23 +0200

    Add images public folder

--- pages/slider.js
+++ pages/slider.js
@@ -1,6 +1,9 @@
 import React from 'react';
 var $ = require('jquery');
 import Image from 'next/image';
+import ocean from '../public/images/ocean-g2ba1bb5c0_1280.jpg';
+import sea from '../public/images/sea-g879f4f00d_1280.jpg';
+import woman from '../public/images/woman-g0b7a7f5dd_1280.jpg';
 
 if (typeof window !== 'undefined') {
   window.$ = window.jQuery = require('jquery');
@@ -8,10 +11,6 @@ if (typeof window !== 'undefined') {
 import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel/dist/assets/owl.theme.default.css';
 import dynamic from 'next/dynamic';
-
-const myLoader = ({ src, width, quality }) => {
-  return `https://pixabay.com//${src}`;
-};
 
 const OwlCarousel = dynamic(() => import('react-owl-carousel'), {
   ssr: false,
@@ -30,26 +29,24 @@ export default function Slider() {
       <OwlCarousel className="owl-theme" {...options}>
         <div className="item">
           <Image
-            loader={myLoader}
-            src="get/g6b3df644c65296486b3ee03597bccf48cf74f4b0a9295fd39715ad9abe1dfe463b7662a93097ccb1b47acb42060466452365f1aaeda4f767d8f19446d1cd4ae250a759cfe2eee7e8b6a33c6da93955ab_1280.jpg"
-            alt="Picture of the author"
+            src={ocean}
+            alt="Ocean"
             width={1280}
             height={853}
           />
         </div>
         <div className="item">
           <Image
-            loader={myLoader}
-            src="get/g0ddd42e68bd52fdd873fb37ae74e5acd915c117facbbcbe88f9166353aca80fb0bbce2c7a3f558e69875dd24b0eef3b2d7d372b3f3fd7c2b7b4c6111b370a88094aa16af377777d64f510c8cb07e0cfd_1280.jpg"
-            alt="Picture of the author"
+            src={sea}
+            alt="Sea"
             width={1280}
             height={853}
           />
         </div>
         <div className="item">
           <Image
-            loader={myLoader}
-            src="get/ga3e87073140dac7eada7ac8671b5643f9dcaf490616fe566a902bf523b1ce436093f83ad6c14832ee8f8673855702f4664c94566c76b7787dec7089eabe833458253e721d98d375a9ad2db3003a5b861_1280.jpg"
+            src={woman}
+            alt="Woman"
             width={1280}
             height={853}
           />
